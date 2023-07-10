32c90a4d148c1e55573ed7f718996d18baa95ffb
Haobo Yang <hobart.yang@qq.com>
2023-07-10 08:16:19 +0800

    Update server.py
    Follow the api doc requirement

--- src/expose/server.py
+++ src/expose/server.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 from typing import Callable, Any
 
-
 class Server:
     """
     A Server class to handle text processing and feedback reception via a simple Flask-based API.
@@ -15,20 +14,29 @@ class Server:
         :param function: The function to be used for processing text.
         """
         self.app = Flask(__name__)
-        CORS(self.app)  # FIXME This is not allowed for local app
+        CORS(self.app)
 
         self.app.route("/api/text", methods=['POST'])(self.post_text)
         self.app.route("/api/feedback", methods=['POST'])(self.post_feedback)
-        self.api_key = "secret_api_key"  # In reality, this should be securely stored
+        self.app.route("/api/history", methods=['DELETE'])(self.delete_history)
+        self.app.route("/api/history", methods=['POST'])(self.post_history)
+        self.app.route("/api/chat", methods=['POST'])(self.post_chat)
+        self.app.route("/api/role", methods=['POST'])(self.post_role)
+        self.app.route("/api/chat/continue", methods=['POST'])(self.post_continue_chat)
+
         self.function = function
 
-    def check_auth(self) -> bool:
+        # Initialize the histories and role as dictionaries
+        self.histories = {}
+        self.roles = {}
+
+    def check_auth(self, role: str) -> bool:
         """
         Check the request for correct authorization.
 
         :return: Whether the request is authorized.
         """
-        return request.headers.get('X-API-Key') == self.api_key
+        return role in self.roles
 
     def post_text(self) -> Any:
         """
@@ -36,18 +44,14 @@ class Server:
 
         :return: A JSON response containing the result of processing the request's text.
         """
-        if not self.check_auth():
-            abort(401)  # Unauthorized
         data = request.get_json()
+        role = data.get('role')
+        if not self.check_auth(role):
+            abort(401)  # Unauthorized
         text = data.get('text')
-        user_feedback = data.get('user_feedback')
-        resource_requirements = data.get('resource_requirements')
 
         if text:
-            # try:
-            result = self.function(text)  # Process the text FIXME ADD TRY EXCEPT
-            # except:
-            #     return jsonify({"error": "Text process error"}), 500
+            result = self.function(text)  # Process the text
             return jsonify({"result": result}), 200
         else:
             return jsonify({"error": "Invalid request parameter"}), 400
@@ -58,13 +62,84 @@ class Server:
 
         :return: A JSON response confirming receipt of feedback.
         """
-        if not self.check_auth():
-            abort(401)  # Unauthorized
         data = request.get_json()
+        role = data.get('role')
+        if not self.check_auth(role):
+            abort(401)  # Unauthorized
         feedback = data.get('feedback')
 
         if feedback:
             return jsonify({"message": "Feedback received"}), 200
+        else:
+            return jsonify({"error": "Invalid request parameter"}), 400
+
+    def delete_history(self) -> Any:
+        """
+        Handle a DELETE request to the /api/history endpoint.
+        Clear the chat history.
+        """
+        data = request.get_json()
+        role = data.get('role')
+        if not self.check_auth(role):
+            abort(401)  # Unauthorized
+        self.histories[role] = []
+        return jsonify({"message": "History cleared"}), 200
+
+    def post_history(self) -> Any:
+        """
+        Handle a POST request to the /api/history endpoint.
+        Returns the chat history.
+        """
+        data = request.get_json()
+        role = data.get('role')
+        if not self.check_auth(role):
+            abort(401)  # Unauthorized
+        return jsonify({"history": self.histories[role]}), 200
+
+    def post_chat(self) -> Any:
+        """
+        Handle a POST request to the /api/chat endpoint.
+        Creates a new chat.
+        """
+        data = request.get_json()
+        role = data.get('role')
+        if role:
+            self.roles[role] = True  # Add the role to the list of valid roles
+            self.histories[role] = []  # Initialize the history for this role
+            return jsonify({"message": f"New chat created with role {role}"}), 200
+        else:
+            return jsonify({"error": "Invalid request parameter"}), 400
+
+    def post_role(self) -> Any:
+        """
+        Handle a POST request to the /api/role endpoint.
+        Changes the AI role.
+        """
+        data = request.get_json()
+        role = data.get('role')
+        if role:
+            self.roles[role] = True
+            return jsonify({"message": f"Role changed to {role}"}), 200
+        else:
+            return jsonify({"error": "Invalid request parameter"}), 400
+
+    def post_continue_chat(self) -> Any:
+        """
+        Handle a POST request to the /api/chat/continue endpoint.
+        Continues a chat based on the history.
+        """
+        data = request.get_json()
+        role = data.get('role')
+        if not self.check_auth(role):
+            abort(401)  # Unauthorized
+        text = data.get('text')
+        if text:
+            # Process the text
+            result = self.function(text)
+            # Update the history
+            self.histories[role].append({'sender': 'user', 'message': text})
+            self.histories[role].append({'sender': 'AI', 'message': result})
+            return jsonify({"response": result}), 200
         else:
             return jsonify({"error": "Invalid request parameter"}), 400
 
@@ -84,7 +159,6 @@ if __name__ == '__main__':
         :return: The processed text.
         """
         return data  # In reality, this would be replaced by actual processing logic
-
 
     server = Server(process_func)
     server.run("localhost", 10086)
